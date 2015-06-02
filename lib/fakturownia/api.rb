b6da4606771ee0784105acdfb73a852c4d544569
Bogusław Tolarz <btolarz@gmail.com>
2015-06-02 14:57:24 +0200

    When error return response not nil

--- lib/fakturownia/api.rb
+++ lib/fakturownia/api.rb
@@ -7,7 +7,7 @@ module Fakturownia
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices/#{invoice_id}.json"
       response = self.make_get_request(endpoint)
 
-      (response.code == '200') ? JSON.parse(response.body) : nil 
+      (response.code == '200') ? JSON.parse(response.body) : nil
     end
 
     # Get JSON-formatted invoice based on order id
@@ -15,7 +15,7 @@ module Fakturownia
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices"
       response = self.make_get_request(endpoint, { :oid => order_id })
 
-      (response.code == '200') ? JSON.parse(response.body) : nil 
+      (response.code == '200') ? JSON.parse(response.body) : nil
     end
 
     # Get JSON-formatted invoices based from given period
@@ -30,36 +30,36 @@ module Fakturownia
       end
       response = self.make_get_request(endpoint, additional_params)
 
-      (response.code == '200') ? JSON.parse(response.body) : nil 
+      (response.code == '200') ? JSON.parse(response.body) : response
     end
 
     # Get invoice PDF based on invoice id
     def self.pdf invoice_id
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices/#{invoice_id}.pdf"
       response = self.make_get_request(endpoint)
-       
-      (response.code == '200') ? response.body : nil
+
+      (response.code == '200') ? response.body : response
     end
 
 
     # Create new invoice
     def self.create invoice_json
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices.json"
-      uri = URI.parse(endpoint) 
+      uri = URI.parse(endpoint)
 
-      json_params = { 
+      json_params = {
         "api_token" => Fakturownia.api_token,
         "invoice" => invoice_json
       }
 
-      request = Net::HTTP::Post.new(uri.path) 
-      request.body = JSON.generate(json_params) 
-      request["Content-Type"] = "application/json" 
-      http = Net::HTTP.new(uri.host, uri.port) 
+      request = Net::HTTP::Post.new(uri.path)
+      request.body = JSON.generate(json_params)
+      request["Content-Type"] = "application/json"
+      http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      response = http.start {|h| h.request(request)} 
-      (response.code == '201') ? JSON.parse(response.body) : nil
+      response = http.start {|h| h.request(request)}
+      (response.code == '201') ? JSON.parse(response.body) : response
     end
 
 
@@ -67,32 +67,32 @@ module Fakturownia
     # invoice_id - id of invoice from fakturownia
     def self.update invoice_id, invoice_json
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices/#{invoice.fakturownia_id}.json"
-      uri = URI.parse(endpoint) 
+      uri = URI.parse(endpoint)
 
-      json_params = { 
+      json_params = {
         "api_token" => Fakturownia.api_token,
         "invoice" => invoice_json
       }
 
-      request = Net::HTTP::Put.new(uri.path) 
-      request.body = JSON.generate(json_params) 
-      request["Content-Type"] = "application/json" 
-      http = Net::HTTP.new(uri.host, uri.port) 
+      request = Net::HTTP::Put.new(uri.path)
+      request.body = JSON.generate(json_params)
+      request["Content-Type"] = "application/json"
+      http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      response = http.start {|h| h.request(request)} 
+      response = http.start {|h| h.request(request)}
 
-      (response.code == '201') ? JSON.parse(response.body) : nil
+      (response.code == '201') ? JSON.parse(response.body) : response
     end
 
 
     # Delete invoice
     def self.delete invoice_id
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices/#{invoice_id}.json"
-      uri = URI.parse(endpoint) 
+      uri = URI.parse(endpoint)
       whole_url = uri.path + "?api_token=#{Fakturownia.api_token}"
 
-      http = Net::HTTP.new(uri.host, uri.port) 
+      http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       http.delete(whole_url)
@@ -125,7 +125,7 @@ module Fakturownia
       (response.code == '200' and response.body.to_s != 'brak klienta') ? response.body : nil
     end
 
-    
+
     # Make GET request returning response
     def self.make_get_request endpoint, additional_params = {}
       uri = URI.parse(endpoint)
@@ -134,8 +134,8 @@ module Fakturownia
         whole_url += "&#{k}=#{v}"
       end
 
-      request = Net::HTTP::Get.new(whole_url) 
-      http = Net::HTTP.new(uri.host, uri.port) 
+      request = Net::HTTP::Get.new(whole_url)
+      http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       http.start {|h| h.request(request)}
